acfdab15b8895c2fb5afdeb45d18ff01e8e9c38d
Lev Stambler <levstamb@gmail.com>
2020-12-20 19:24:00 -0500

    add path for imports

--- translate.py
+++ translate.py
@@ -18,6 +18,9 @@ import argparse
 import os
 import sys
 
+parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_name + "/TransCoder")
+
 import fastBPE
 from bunch import bunchify
 import torch
