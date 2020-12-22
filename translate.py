ef503029a346331c17db6741e7c8d9fa99d3ecdc
Lev Stambler <levstamb@gmail.com>
2020-12-22 10:12:02 -0500

    assert BPE callable

--- translate.py
+++ translate.py
@@ -24,6 +24,7 @@ import torch
 
 parent_dir_name = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir_name + "/TransCoder")
+assert(callable(fastBPE.fastBPE))
 
 import preprocessing.src.code_tokenizer as code_tokenizer
 from XLM.src.data.dictionary import Dictionary, BOS_WORD, EOS_WORD, PAD_WORD, UNK_WORD, MASK_WORD
